33733573dd6a808dec0174eaf360f4e656e96555
Simon Esders <simonesders2003@gmail.com>
2023-11-30 22:38:09 +0100

    skills added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { IntroductionComponent } from "../introduction/introduction.component";
 import { AboutmeComponent } from "../aboutme/aboutme.component";
+import { SkillsComponent } from "../skills/skills.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [HeaderComponent, IntroductionComponent, AboutmeComponent]
+    imports: [HeaderComponent, IntroductionComponent, AboutmeComponent, SkillsComponent]
 })
 export class HomeComponent {
 
