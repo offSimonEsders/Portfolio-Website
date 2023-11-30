acea681fb18221904f0372a693cfdf43e45aad1b
Simon Esders <simonesders2003@gmail.com>
2023-11-30 20:18:08 +0100

    introduction layout and style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { IntroductionComponent } from "../introduction/introduction.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [HeaderComponent]
+    imports: [HeaderComponent, IntroductionComponent]
 })
 export class HomeComponent {
 
