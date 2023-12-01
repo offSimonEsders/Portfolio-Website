aaaf3c91276309d45dfa592852e473e7efaa23cb
Simon Esders <simonesders2003@gmail.com>
2023-12-01 23:28:11 +0100

    add skills with ngFor

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
+  skills = ['Angular', 'Typescript', 'Javascript', 'HTML', 'Firebase', 'Git', 'CSS', 'Rest-Api', 'Scrum', 'Material design'];
 }
