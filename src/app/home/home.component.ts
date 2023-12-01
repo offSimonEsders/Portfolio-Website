8518f5ce91b5a03cf56bf78abdf73586437f32a7
Simon Esders <simonesders2003@gmail.com>
2023-12-02 00:29:17 +0100

    skills style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,14 @@ import { HeaderComponent } from "../header/header.component";
 import { IntroductionComponent } from "../introduction/introduction.component";
 import { AboutmeComponent } from "../aboutme/aboutme.component";
 import { SkillsComponent } from "../skills/skills.component";
+import { PortfolioComponent } from "../portfolio/portfolio.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [HeaderComponent, IntroductionComponent, AboutmeComponent, SkillsComponent]
+    imports: [HeaderComponent, IntroductionComponent, AboutmeComponent, SkillsComponent, PortfolioComponent]
 })
 export class HomeComponent {
 
