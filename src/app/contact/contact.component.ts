5b724dfd490638411a6ab3686b68d8b6edae1dd8
Simon Esders <simonesders2003@gmail.com>
2023-12-02 18:31:00 +0100

    form style

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements AfterViewInit {
+
+  privacypolicycheck!: HTMLInputElement;
+  submitbutton!: HTMLButtonElement;
+
+  constructor() {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.privacypolicycheck = <HTMLInputElement>document.getElementById('privacy-policy-check');
+    this.submitbutton = <HTMLButtonElement>document.querySelector('.submit-button');
+  }
+
+  state() {
+    if (this.privacypolicycheck.checked === true) {
+      this.submitbutton.removeAttribute('disabled');
+      this.submitbutton.classList.remove('privacy-policy-unchecked');
+    } else {
+      this.submitbutton.setAttribute('disabled', 'true');
+      this.submitbutton.classList.add('privacy-policy-unchecked');
+    }
+  }
 
 }
