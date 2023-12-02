ba5e4bb9ec209bf81b43679cc2064c7c55886f01
Simon Esders <simonesders2003@gmail.com>
2023-12-02 22:30:31 +0100

    send mail

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -11,6 +11,9 @@ export class ContactComponent implements AfterViewInit {
 
   privacypolicycheck!: HTMLInputElement;
   submitbutton!: HTMLButtonElement;
+  contactform!: HTMLFormElement;
+  contactforminputcontainer: any;
+  sendmailresponse!: HTMLDivElement;
 
   constructor() {
 
@@ -19,6 +22,9 @@ export class ContactComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     this.privacypolicycheck = <HTMLInputElement>document.getElementById('privacy-policy-check');
     this.submitbutton = <HTMLButtonElement>document.querySelector('.submit-button');
+    this.contactform = <HTMLFormElement>document.querySelector('.contact-form');
+    this.contactforminputcontainer = document.querySelector('.contact-form-input-container');
+    this.sendmailresponse = <HTMLDivElement>document.querySelector('.send-mail-response');
   }
 
   state() {
@@ -29,6 +35,69 @@ export class ContactComponent implements AfterViewInit {
       this.submitbutton.setAttribute('disabled', 'true');
       this.submitbutton.classList.add('privacy-policy-unchecked');
     }
+  }
+
+  async sendmail(event: any) {
+    event.preventDefault();
+    let fd = new FormData(event.target);
+    let response = await this.fetchFormspree(fd);
+    this.clearForm();
+    this.disableForm();
+    await this.checkResponse(response);
+    this.enableForm();
+  }
+
+  clearForm() {
+    this.contactform.reset();
+  }
+
+  disableForm() {
+    this.contactforminputcontainer.childNodes.forEach((element: any) => {
+      element.setAttribute('disabled', 'true');
+    });
+    this.submitbutton.setAttribute('disabled', 'true');
+    this.privacypolicycheck.setAttribute('disabled', 'true');
+  }
+
+  enableForm() {
+    this.contactforminputcontainer.childNodes.forEach((element: any) => {
+      element.removeAttribute('disabled');
+    });
+    this.submitbutton.removeAttribute('disabled');
+    this.privacypolicycheck.removeAttribute('disabled');
+  }
+
+  checkResponse(response: any) {
+    if (response.ok) {
+      this.responseOK();
+    } else {
+      this.responseError();
+    }
+  }
+
+  async fetchFormspree(fd: FormData) {
+    return await fetch('https://formspree.io/f/moqoozva', {
+      method: 'POST', body: fd, headers: {
+        'Accept': 'application/json'
+      }
+    });
+  }
+
+  responseOK() {
+    this.sendmailresponse.textContent = 'Message sent successfully!';
+    this.playAnimation();
+  }
+
+  responseError() {
+    this.sendmailresponse.textContent = 'Message could not be sent!';
+    this.playAnimation();
+  }
+
+  playAnimation() {
+    this.sendmailresponse.style.display = 'flex';
+    setTimeout(() => {
+      this.sendmailresponse.style.display = 'none';
+    }, 4000);
   }
 
 }
