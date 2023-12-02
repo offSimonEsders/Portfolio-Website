99cf4718a0b47789b11d158bbc49cb6be8e28a6a
Simon Esders <simonesders2003@gmail.com>
2023-12-02 02:07:54 +0100

    new component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,13 +4,14 @@ import { IntroductionComponent } from "../introduction/introduction.component";
 import { AboutmeComponent } from "../aboutme/aboutme.component";
 import { SkillsComponent } from "../skills/skills.component";
 import { PortfolioComponent } from "../portfolio/portfolio.component";
+import { ContactComponent } from "../contact/contact.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [HeaderComponent, IntroductionComponent, AboutmeComponent, SkillsComponent, PortfolioComponent]
+    imports: [HeaderComponent, IntroductionComponent, AboutmeComponent, SkillsComponent, PortfolioComponent, ContactComponent]
 })
 export class HomeComponent {
 
