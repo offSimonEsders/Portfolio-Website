45e907639f5b78916a9fecea81e1c10e076478ec
Simon Esders <simonesders2003@gmail.com>
2023-12-03 01:02:24 +0100

    Imprint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'Imprint', component: ImprintComponent, pathMatch: 'full'}
 ];
