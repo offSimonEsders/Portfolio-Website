69d2a95aea9848d34a2ba98169385958d2b5f9fe
Simon Esders <simonesders2003@gmail.com>
2023-12-03 18:21:04 +0100

    scrollto footer and contact

--- src/app/introduction/introduction.component.ts
+++ src/app/introduction/introduction.component.ts
@@ -8,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './introduction.component.scss'
 })
 export class IntroductionComponent {
+  
+  scrollToContact() {
+    const element = document.getElementById('contact');
+    if (element) {
+      const Coordinate = element.getBoundingClientRect().top + window.pageYOffset - 150;
+      window.scrollTo({ top: Coordinate });
+    }
+  }
+
+  scrollDown() {
+    const element = document.getElementById('footer');
+    if (element) {
+      element.scrollIntoView({ block: 'start' });
+    }
+  }
 
 }
