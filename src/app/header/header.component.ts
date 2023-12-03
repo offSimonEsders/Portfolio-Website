b15983cec77578f32677c7e7c52a1353c7e2e2e8
Simon Esders <simonesders2003@gmail.com>
2023-12-03 02:16:34 +0100

    to much

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +10,22 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(private router: Router) { }
+
+  scrollToTop() {
+    this.setRoute();
+    window.scrollTo({ top: 0 });
+  }
+
   scrollToAbout() {
     let element = document.getElementById('aboutme');
     if (element) {
       element.scrollIntoView({ block: 'start' });
+    } else {
+      this.setRoute();
+      setTimeout(() => {
+        this.scrollToAbout();
+      }, 100);
     }
   }
 
@@ -21,6 +34,11 @@ export class HeaderComponent {
     if (element) {
       const Coordinate = element.getBoundingClientRect().top + window.pageYOffset - 250;
       window.scrollTo({ top: Coordinate });
+    } else {
+      this.setRoute();
+      setTimeout(() => {
+        this.scrollToSkills();
+      }, 100);
     }
   }
 
@@ -29,7 +47,16 @@ export class HeaderComponent {
     if (element) {
       const Coordinate = element.getBoundingClientRect().top + window.pageYOffset - 150;
       window.scrollTo({ top: Coordinate });
+    } else {
+      this.setRoute();
+      setTimeout(() => {
+        this.scrollToProjects();
+      }, 100);
     }
+  }
+
+  setRoute() {
+    this.router.navigate(['/']);
   }
 
 }
--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
-  selector: 'app-imprint',
-  standalone: true,
-  imports: [],
-  templateUrl: './imprint.component.html',
-  styleUrl: './imprint.component.scss'
+    selector: 'app-imprint',
+    standalone: true,
+    templateUrl: './imprint.component.html',
+    styleUrl: './imprint.component.scss',
+    imports: [HeaderComponent]
 })
 export class ImprintComponent {
+  constructor(private router:Router) {
+
+  }
+
+  navigateHome() {
+    this.router.navigate(['/']);
+  }
 
 }
