22856e8af6a25a2a5993e908a444a94637c804f3
Simon Esders <simonesders2003@gmail.com>
2023-12-07 18:27:08 +0100

    new rout and component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { DsgvoComponent } from './dsgvo/dsgvo.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: 'full' },
-    { path: 'Imprint', component: ImprintComponent, pathMatch: 'full'}
+    { path: 'Imprint', component: ImprintComponent, pathMatch: 'full'},
+    { path:'DSGVO', component: DsgvoComponent, pathMatch: 'full' }
 ];
