e758813f8fc0b594e9619c05b28c5eca7cb6570f
Simon Esders <simonesders2003@gmail.com>
2023-12-07 21:28:35 +0100

    made about-me responsive

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-aboutme',
@@ -7,6 +7,29 @@ import { Component } from '@angular/core';
   templateUrl: './aboutme.component.html',
   styleUrl: './aboutme.component.scss'
 })
-export class AboutmeComponent {
+export class AboutmeComponent implements AfterViewInit {
+
+  aboutmeframe!: HTMLDivElement;
+  aboutmetext!: HTMLDivElement;
+  bigimgframe!: HTMLDivElement;
+
+  constructor() {
+
+  }
+  ngAfterViewInit(): void {
+    this.aboutmeframe = <HTMLDivElement>document.querySelector('.aboutme-frame');
+    this.aboutmetext = <HTMLDivElement>document.querySelector('.aboutme-text');
+    this.bigimgframe = <HTMLDivElement>document.querySelector('.big-img-frame');
+    this.setFrameHeight();
+    
+    window.addEventListener('resize', () => {
+      this.setFrameHeight();
+    });
+
+  }
+
+  setFrameHeight() {
+    this.aboutmeframe.style.height = (this.aboutmetext.offsetHeight + this.bigimgframe.offsetHeight) + 'px';
+  }
 
 }
